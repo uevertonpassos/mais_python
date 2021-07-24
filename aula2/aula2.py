f12129cac5f47ac9787b7251afd523effa2d75e2
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-24 11:40:58 -0300

    Create aula2.py

---
+++ aula2/aula2.py
@@ -0,0 +1,5 @@
+"""
+TESTANDO COMANDO PRINT E SEPARANDO CPF USANDO O PRÓPRIO PRINT
+"""
+print('123', '456', '789', sep='.', end='-')  # sep é para escolher o que separa os prints, end escolhe o que vem no final da linha caso haja um segundo print
+print('01')
