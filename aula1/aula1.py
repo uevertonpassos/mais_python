979e5664798b50de2ca4d8d810896ce92de0df36
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-24 11:00:02 -0300

    Create aula1.py

---
+++ aula1/aula1.py
@@ -0,0 +1,5 @@
+#  Comentando Códigos
+#  Para comentar um código usa-se "#" ex: print('Olá, mundo!')  # deste jeito
+
+print('Olá, mundo!')  # deste jeito
+#  Sempre com dois espaços após a linha de código
