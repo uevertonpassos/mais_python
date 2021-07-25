931a8fe70eb61e19336afd705889a89a66a88330
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-25 01:37:13 -0300

    Create aula3.py

---
+++ aula3/aula3.py
@@ -0,0 +1,8 @@
+# Testando jeitos diferentes de strings em print
+
+print('Esse tipo de Strings')
+print('Esse tipo de \nStrings')  # colocar \n quebra a linha
+print('Esse "tipo" de Strings')  # colocar  aspas duplas, dentro de as pas simples, faaz com que aspas apareçam no texto
+print("Esse 'tipo' de Strings")  # o mesmo para aspas simples
+# mais formas de colocar  aspas no print
+n = int(input())
