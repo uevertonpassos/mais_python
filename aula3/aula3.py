c3e59146171d21f6a73bc0fa818f34d38e39be65
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-30 02:14:20 -0300

    Aula três finalizada

--- aula3/aula3.py
+++ aula3/aula3.py
@@ -5,5 +5,3 @@ print('Esse tipo de \nStrings')  # colocar \n quebra a linha
 print('Esse "tipo" de Strings')  # colocar  aspas duplas, dentro de as pas simples, faaz com que aspas apareçam no texto
 print("Esse 'tipo' de Strings")  # o mesmo para aspas simples
 # mais formas de colocar  aspas no print
-print('Estou testando commit pelo vscode')
-print('De novo')
